169f088ac6e65148717743a361cde02c6d25d5ae
Sandeep Acharya <i.am.sandeep.acharya@gmail.com>
2016-08-15 02:42:50 +0530

    Update main.js

--- config/main.js
+++ config/main.js
@@ -3,11 +3,11 @@ module.exports = {
       'secret': 'qwytywhjsks',
       'database': 'mongodb://127.0.0.1:27017/society' ,
       'aws': {
-        'accessKeyId': 'AKIAJQ66SOY2N3RPPO5A',
-        'secretAccessKey': '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',
-        'region': 's3-ap-southeast-1',
-        'imageBucketName': 'sayanriju-smart-images',
-        'documentBucketName': 'sayanriju-smart'
+        'accessKeyId': ',
+        'secretAccessKey': '',
+        'region': '',
+        'imageBucketName': '',
+        'documentBucketName': ''
       }
     }
 };
